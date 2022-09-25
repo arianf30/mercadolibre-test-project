e27245858f443868e481cd091f7eae63f5f83937
Arián <arianfernandez@gmail.com>
2022-09-25 17:19:38 -0300

    feat: add getItemById in router

--- src/main/routes/item-routes.ts
+++ src/main/routes/item-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { AdapterRoute } from '../adapters/express-adapter'
-import { makeFindItemsController } from '../factories/item'
+import { makeFindItemsController, makeGetItemByIdController } from '../factories/item'
 
+/* eslint-disable @typescript-eslint/no-misused-promises */
 export default (router: Router): void => {
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.get('/items', AdapterRoute(makeFindItemsController()))
+  router.get('/items/:id', AdapterRoute(makeGetItemByIdController()))
 }
