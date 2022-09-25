1ebf37c169294bb9523f18786af2e7a479a5c7ad
Arián <arianfernandez@gmail.com>
2022-09-25 13:20:10 -0300

    chore: fix lint error  adapter

--- src/main/routes/item-routes.ts
+++ src/main/routes/item-routes.ts
@@ -3,5 +3,6 @@ import { AdapterRoute } from '../adapters/express-adapter'
 import { makeFindItemsController } from '../factories/item'
 
 export default (router: Router): void => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.get('/items', AdapterRoute(makeFindItemsController()))
 }
