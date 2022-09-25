f11080e20ad4887b0b4d5cad1c3bc55f62f63af4
Arián <arianfernandez@gmail.com>
2022-09-25 13:18:32 -0300

    chore: fix adapter

--- src/main/routes/item-routes.ts
+++ src/main/routes/item-routes.ts
@@ -3,11 +3,5 @@ import { AdapterRoute } from '../adapters/express-adapter'
 import { makeFindItemsController } from '../factories/item'
 
 export default (router: Router): void => {
-  // router.get('/items', (req, res) => {
-  //   res.status(200).json({
-  //     ok: true,
-  //     message: 'Ruta activa'
-  //   })
-  // })
   router.get('/items', AdapterRoute(makeFindItemsController()))
 }
