be787b0a72418292e8f6acc085e21297c75f64b0
Arián <arianfernandez@gmail.com>
2022-09-26 15:08:29 -0300

    chore: add static route

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import { config as dotenv } from 'dotenv'
+import staticRoute from './static-route'
 import middlewares from './middlewares'
 import routes from './routes'
 
 const app = express()
+// Public folder
+staticRoute(app)
 // Middlewares Init
 middlewares(app)
 // Routes Init
