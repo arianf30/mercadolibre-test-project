76d6ccebe79e3bcf54176e59431ad506ecb83b0d
Arián <arianfernandez@gmail.com>
2022-09-26 15:11:37 -0300

    chore: add _app to client render

---
+++ src/client/_app.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import Router from './router/Router'
+
+const root = document.getElementById('root') as HTMLElement
+const initialData = window && window.__INITIAL_DATA__
+
+ReactDOM.hydrate(
+  <div>
+    <BrowserRouter>
+      <Router ssProps={initialData.props} isBot={initialData.isBot} />
+    </BrowserRouter>
+  </div>,
+  root
+)
